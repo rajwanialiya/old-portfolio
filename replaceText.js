0cb17adc0f5c00ab86fe0c5ba6e7a61a8b7c48a8
rajwanialiya <rajwani.aliya@gmail.com>
2019-05-25 14:21:18 -0400

    replaceText.js updated

--- replaceText.js
+++ replaceText.js
@@ -2,19 +2,19 @@ $(document).ready(function(){
     // var replace = document.getElementById('replace')
 
     //I am interested in...
-    var one = "creating solutions to global issues with a holistic mindset"; 
+    var one = "using technology to address global issues"; 
     var two = "developing an interdisciplinary skillset"
-    var three = "building solutions that strengthen our relationship with technology"; 
-    var four = "developing strong responsive and interactive design"; 
-    var five = "exploring the use of technology to ________"
+    // var three = "improving our relationship with technology"; 
+    var four = "producing strong responsive and interactive design"; 
+    var five = "exploring the use of technology to develop holistic solutions"
 
-    var text = [one, two, three, four, five]
+    var text = [one, two, four, five]
 
     var typed = new Typed("#typed", {
         strings: text,
         typeSpeed: 15,
         backSpeed: 15,
-        backDelay: 2500,
+        backDelay: 3000,
         startDelay: 1000,
         loop: true,
         showCursor: true,
